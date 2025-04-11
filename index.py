c1bfefbfb9558384b1c0231f55638ad1ddcba6dc
meenakhose <meena.khose881@deccansociety.org>
2025-04-11 17:09:31 +0530

    index.py chngs

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+'''from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 from typing import Annotated, Optional, Dict
 from scraper import scrap_meshoo
@@ -14,4 +14,20 @@ def scrape(
     base_url: Annotated[str, Query(description="Base URL to scrape")],
     pages: Annotated[int, Query(1, description="Number of pages to scrape")],
 ) -> Dict:
+    return scrap_meshoo(base_url=base_url, pages=pages)'''
+from typing import Annotated
+from fastapi import FastAPI, Query
+from scraper import scrap_meshoo
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "FastAPI deployed successfully!"}
+
+@app.get("/scrape")
+def scrape(
+    base_url: Annotated[str, Query(description="Base URL to scrape")],
+    pages: Annotated[int, Query(default=1, description="Number of pages to scrape")]
+) -> dict:
     return scrap_meshoo(base_url=base_url, pages=pages)
